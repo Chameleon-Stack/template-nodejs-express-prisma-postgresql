ddb143bab47126322320f0a203f2baaf3d4d8c99
ibsiany <ibsianyd@gmail.com>
2023-11-29 01:29:33 -0300

    feat: init project

---
+++ src/modules/categories/infra/prisma/repositories/CategoryRepository.ts
@@ -0,0 +1,34 @@
+import { prisma } from '../../../../../prisma';
+import { ICategory } from '../../../dtos/ICategory';
+import { ICategoryRepository } from '../../../repositories/ICategoryRepository';
+
+export class CategoryRepository implements ICategoryRepository {
+  async create(name: string, user: any): Promise<ICategory> {
+    return prisma.category.create({
+      data: {
+        name,
+        User: user,
+        user_id: user.id,
+      },
+    });
+  }
+
+  async findAll(user_id: string, name?: string): Promise<ICategory[]> {
+    return prisma.category.findMany({
+      where: {
+        user_id,
+        name: name ? { contains: name.toLowerCase() } : undefined,
+      },
+    });
+  }
+
+  async findById(id: string): Promise<ICategory | null> {
+    return prisma.category.findFirst({ where: { id } });
+  }
+
+  async delete(category: ICategory): Promise<void> {
+    await prisma.category.delete({
+      where: { id: category.id },
+    });
+  }
+}
